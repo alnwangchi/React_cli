268e01dc40578800c8a2f71989d50d74a795bc8b
“Allen” <alnwangchi@gmail.com>
2021-12-23 00:08:14 +0800

    將 axios 改用 fetch and async await 寫看看

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
-import SearchField from './components/SearchField';
-import ResultField from './components/ResultField';
-
-import './App.css'
 
 class App extends Component {
   render() {
     return ( 
-      <div className="github_app">
-        <SearchField /> 
-        <ResultField /> 
+      <div>
+        App
       </div>
     );
   }
